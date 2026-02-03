b2df9ff199a672bb2f56e6098e2a2d10e23a6af6
Younes Haddad <younes.haddad@epitech.eu>
2026-02-03 15:46:27 +0100

    feat(channels): add get channel details endpoint

--- backend/src/modules/channels/channels.schemas.ts
+++ backend/src/modules/channels/channels.schemas.ts
@@ -1,14 +1,21 @@
 import { z } from "zod";
- 
+
+//Validate serverId from URL from params
 export const serverIdParamsSchema = z.object({
   serverId: z.string().min(10, "serverId is required"),
 });
- 
+
+//Validate channel name from body
 export const createChannelBodySchema = z.object({
   name: z.string().min(1, "1 character min for the name").max(15, "15 characters max for the name"),
 });
- 
- 
+
+//Validate user-id from header
 export const userIdHeaderSchema = z.object({
   "user-id": z.string().uuid("user-id must be a valid UUID"),
+});
+
+//Validate channelId from params
+export const channelIdParamsSchema = z.object({
+  channelId: z.string().uuid("channelId must be a valid UUID"),
 });
