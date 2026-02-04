4dd40c34eefa1e7806734506c94f86eb45cb95f9
Younes Haddad <younes.haddad@epitech.eu>
2026-02-04 11:22:24 +0100

    feat(channels): protect channels routes with auth middleware

--- backend/src/modules/channels/channels.router.ts
+++ backend/src/modules/channels/channels.router.ts
@@ -1,15 +1,17 @@
 import { Router } from "express";
 import asyncHandler from "../../shared/utils/asyncHandler.js";
 import { createChannelController, getServerChannelsController, getChannelDetails, deleteChannelController, updateChannelController } from "./channels.controller.js";
- 
+import { requireAuth } from "../../shared/middlewares/auth.middleware.js";
+
+
 export const channelsRouter = Router();
  
-channelsRouter.post("/servers/:serverId/channels",asyncHandler(createChannelController));
+channelsRouter.post("/servers/:serverId/channels", requireAuth, asyncHandler(createChannelController));
  
-channelsRouter.get("/servers/:serverId/channels",asyncHandler(getServerChannelsController));
+channelsRouter.get("/servers/:serverId/channels", requireAuth, asyncHandler(getServerChannelsController));
 
-channelsRouter.get("/channels/:channelId", asyncHandler(getChannelDetails));
+channelsRouter.get("/channels/:channelId", requireAuth, asyncHandler(getChannelDetails));
 
-channelsRouter.delete("/channels/:channelId",asyncHandler(deleteChannelController));
+channelsRouter.delete("/channels/:channelId", requireAuth, asyncHandler(deleteChannelController));
 
-channelsRouter.put("/channels/:channelId",asyncHandler(updateChannelController));
+channelsRouter.put("/channels/:channelId", requireAuth, asyncHandler(updateChannelController));
